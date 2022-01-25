201835b7c820feaa7ee61bfeee70a42ce48469ca
Jim Boone <jim@unibrain.org>
2022-01-24 16:41:51 -0800

    Added site description and list of reports

--- index.php
+++ index.php
@@ -1,32 +1,30 @@
 <?php
-if(file_exists("install/index.php")){
+if (file_exists("install/index.php")) {
 	//perform redirect if installer files exist
 	//this if{} block may be deleted once installed
 	header("Location: install/index.php");
 }
 
 require_once 'users/init.php';
-require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
-if(isset($user) && $user->isLoggedIn()){
+require_once $abs_us_root . $us_url_root . 'users/includes/template/prep.php';
+if (isset($user) && $user->isLoggedIn()) {
 }
 ?>
-		<div class="jumbotron">
-			<h1 align="center"><?=lang("JOIN_SUC");?> <?php echo $settings->site_name;?></h1>
-			<p align="center" class="text-muted"><?=lang("MAINT_OPEN")?></p>
-			<p align="center">
-				<?php
-				if($user->isLoggedIn()){?>
-					<a class="btn btn-primary" href="users/account.php" role="button"><?=lang("ACCT_HOME");?> &raquo;</a>
-				<?php }else{?>
-					<a class="btn btn-warning" href="users/login.php" role="button"><?=lang("SIGNIN_TEXT");?> &raquo;</a>
-					<a class="btn btn-info" href="users/join.php" role="button"><?=lang("SIGNUP_TEXT");?> &raquo;</a>
-				<?php }?>
-			</p>
-			<br>
-			<p align="center"><?=lang("MAINT_PLEASE");?></p>
-			<h4 align="center"><a href="https://userspice.com/getting-started/">https://userspice.com/getting-started/</a></h4>
-		</div>
-<?php  languageSwitcher();?>
+<div class="jumbotron">
+	<h1 align="center"><?= lang("JOIN_SUC"); ?> <?php echo $settings->site_name; ?></h1>
+	<div class="jumbotron">
+		<ul>
+			<li><a href='app/classifiers.php'>Classifiers</a> - A list of current USPSA classifiers with some intersting information about the setup for each</li>
+			<li><a href='app/section_shot.php'>Section Classifiers</a> - What classifiers the section has run and when</li>
+
+			<?php
+			if ($user->isLoggedIn()) { ?>
+				<li>Section DQ - Reports on section DQ's</li>
+			<?php } ?>
+
+		</ul>
+	</div>
+</div>
 
 
 <!-- Place any per-page javascript here -->
